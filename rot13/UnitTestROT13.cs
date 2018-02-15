a66b1841b1c13f5a77c810f28aa9d6a9d5b0d372
kyo.lai82 <kyo.lai82@titansoft.com.sg>
2018-02-15 11:53:13 +0800

    simple refactor

--- rot13/UnitTestROT13.cs
+++ rot13/UnitTestROT13.cs
@@ -11,7 +11,7 @@ namespace TRPG_KATA_ROT13
         public void Input_a_should_get_n()
         {
             var rotTranslator = new RotTranslator();
-            var result = rotTranslator.tanslate("a");
+            var result = rotTranslator.Tanslate("a");
             Assert.AreEqual("n", result);
         }
 
@@ -19,7 +19,7 @@ namespace TRPG_KATA_ROT13
         public void Input_b_should_get_o()
         {
             var rotTranslator = new RotTranslator();
-            var result = rotTranslator.tanslate("b");
+            var result = rotTranslator.Tanslate("b");
             Assert.AreEqual("o", result);
         }
 
@@ -27,7 +27,7 @@ namespace TRPG_KATA_ROT13
         public void Input_ab_should_get_no()
         {
             var rotTranslator = new RotTranslator();
-            var result = rotTranslator.tanslate("ab");
+            var result = rotTranslator.Tanslate("ab");
             Assert.AreEqual("no", result);
         }
 
@@ -35,7 +35,7 @@ namespace TRPG_KATA_ROT13
         public void Input_Ab_should_get_No()
         {
             var rotTranslator = new RotTranslator();
-            var result = rotTranslator.tanslate("Ab");
+            var result = rotTranslator.Tanslate("Ab");
             Assert.AreEqual("No", result);
         }
 
@@ -43,39 +43,86 @@ namespace TRPG_KATA_ROT13
         public void Input_A12b_should_get_N12o()
         {
             var rotTranslator = new RotTranslator();
-            var result = rotTranslator.tanslate("A12b");
+            var result = rotTranslator.Tanslate("A12b");
             Assert.AreEqual("N12o", result);
+        }
+
+        [TestMethod]
+        public void Input_A1_2b_should_get_N1_2o()
+        {
+            var rotTranslator = new RotTranslator();
+            var result = rotTranslator.Tanslate("A1 2b");
+            Assert.AreEqual("N1 2o", result);
+        }
+
+        [TestMethod]
+        public void Input_case1()
+        {
+            var rotTranslator = new RotTranslator();
+            var result = rotTranslator.Tanslate("EBG13 rknzcyr.");
+            Assert.AreEqual("ROT13 example.", result);
         }
     }
 
     public class RotTranslator
     {
-        public string tanslate(string str)
+        private readonly int _rotRange = 13;
+
+        public string Tanslate(string str)
         {
             var charArray = str.ToCharArray();
             var result = new StringBuilder();
             foreach (var ch in charArray)
             {
-                if (Char.IsDigit(ch))
+                if (!Rot13Case(ch))
                 {
                     result.Append(ch);
                     continue;
                 }
                 var asciiNum = Convert.ToInt32(ch);
-                asciiNum += 13;
+                if (ForwardCase(asciiNum))
+                {
+                    asciiNum += _rotRange;
+                }
+                else
+                {
+                    asciiNum -= _rotRange;
+                }
                 result.Append(Convert.ToChar(asciiNum));
             }
 
             return result.ToString();
+        }
 
-            if (str.Equals("a"))
+        private bool ForwardCase(int asciiNum)
+        {
+            if (asciiNum >= 65 && asciiNum < 78)
             {
-                return "n";
+                return true;
             }
-            else
+
+            if (asciiNum >= 97 && asciiNum < 110)
             {
-                return "o";
+                return true;
             }
+
+            return false;
+        }
+
+        private static bool Rot13Case(char ch)
+        {
+            var asciiNum = Convert.ToInt32(ch);
+            if (asciiNum >= 65 && asciiNum <= 90)
+            {
+                return true;
+            }
+
+            if (asciiNum >= 97 && asciiNum <= 122)
+            {
+                return true;
+            }
+
+            return false;
         }
     }
 }
