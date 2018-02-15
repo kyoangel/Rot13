caea039abae33d3a06121168120ea1806a773486
kyo.lai82 <kyo.lai82@titansoft.com.sg>
2018-02-15 11:12:26 +0800

    hande lower/upper/number case

---
+++ rot13/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("rot13")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("rot13")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4b46161a-61f3-49ca-9db4-5d7f3585b84e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ rot13/UnitTestROT13.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Text;
+
+namespace TRPG_KATA_ROT13
+{
+    [TestClass]
+    public class UnitTestROT13
+    {
+        [TestMethod]
+        public void Input_a_should_get_n()
+        {
+            var rotTranslator = new RotTranslator();
+            var result = rotTranslator.tanslate("a");
+            Assert.AreEqual("n", result);
+        }
+
+        [TestMethod]
+        public void Input_b_should_get_o()
+        {
+            var rotTranslator = new RotTranslator();
+            var result = rotTranslator.tanslate("b");
+            Assert.AreEqual("o", result);
+        }
+
+        [TestMethod]
+        public void Input_ab_should_get_no()
+        {
+            var rotTranslator = new RotTranslator();
+            var result = rotTranslator.tanslate("ab");
+            Assert.AreEqual("no", result);
+        }
+
+        [TestMethod]
+        public void Input_Ab_should_get_No()
+        {
+            var rotTranslator = new RotTranslator();
+            var result = rotTranslator.tanslate("Ab");
+            Assert.AreEqual("No", result);
+        }
+
+        [TestMethod]
+        public void Input_A12b_should_get_N12o()
+        {
+            var rotTranslator = new RotTranslator();
+            var result = rotTranslator.tanslate("A12b");
+            Assert.AreEqual("N12o", result);
+        }
+    }
+
+    public class RotTranslator
+    {
+        public string tanslate(string str)
+        {
+            var charArray = str.ToCharArray();
+            var result = new StringBuilder();
+            foreach (var ch in charArray)
+            {
+                if (Char.IsDigit(ch))
+                {
+                    result.Append(ch);
+                    continue;
+                }
+                var asciiNum = Convert.ToInt32(ch);
+                asciiNum += 13;
+                result.Append(Convert.ToChar(asciiNum));
+            }
+
+            return result.ToString();
+
+            if (str.Equals("a"))
+            {
+                return "n";
+            }
+            else
+            {
+                return "o";
+            }
+        }
+    }
+}
